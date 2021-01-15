7e2ca023bd4834261815a0460c3002bb3356b0c4
Brandon Jones <bcjones1@fullsail.edu>
2021-01-14 19:21:01 -0500

    Add assets for lights, skybox
    Summary:
    
    Changed:
    
    Added:
    
    Removed:

--- Source/CheekyFPS.Target.cs
+++ Source/CheekyFPS.Target.cs
@@ -9,7 +9,7 @@ public class CheekyFPSTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+        bUsesSteam = true;
 		ExtraModuleNames.AddRange( new string[] { "CheekyFPS" } );
 	}
 }
