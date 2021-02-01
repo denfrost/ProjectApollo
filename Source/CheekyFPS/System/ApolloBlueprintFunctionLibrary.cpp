3f24f2ed71955bb50eb932187650842dcbb92629
Brandon Jones <bcjones1@fullsail.edu>
2021-02-01 03:47:59 -0500

    Make minor improvements to template UI, Resolve RSO not loading properly bug, add screenshot capture to saves
    Saves will now take a screenshot after being called for load screen
    Making changes to template UI to provide better UX

--- Source/CheekyFPS/System/ApolloBlueprintFunctionLibrary.cpp
+++ Source/CheekyFPS/System/ApolloBlueprintFunctionLibrary.cpp
@@ -3,3 +3,24 @@
 
 #include "ApolloBlueprintFunctionLibrary.h"
 
+FVector2D UApolloBlueprintFunctionLibrary::GetGameResolution()
+{
+	FVector2D Result = FVector2D(1, 1);
+
+	Result.X = GSystemResolution.ResX;
+	Result.Y = GSystemResolution.ResY;
+
+	return Result;
+}
+
+FVector2D UApolloBlueprintFunctionLibrary::GetGameViewportSize()
+{
+	FVector2D Result = FVector2D(1, 1);
+
+	if (GEngine && GEngine->GameViewport)
+	{
+		GEngine->GameViewport->GetViewportSize( /*out*/Result);
+	}
+
+	return Result;
+}
--- Source/CheekyFPS/System/ApolloBlueprintFunctionLibrary.h
+++ Source/CheekyFPS/System/ApolloBlueprintFunctionLibrary.h
@@ -13,5 +13,16 @@ UCLASS()
 class CHEEKYFPS_API UApolloBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
+
+public:
+
+	//Returns the game's resolution, which is typically at 'native' (a 1920x1080 image on a 1920x1080 screen)
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "System|Resolution")
+	static FVector2D GetGameResolution();
+
+	//Returns the current viewport's screen space, this may not be the same as the display resolution
+	//Resolution may be higher or lower than the actual display
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category = "System|Viewport")
+	static FVector2D GetGameViewportSize();
 	
 };
